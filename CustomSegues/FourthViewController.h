ecce82ecb22f2b2f33f846b2c89475ac6a321aa8
maginawin <maginawin@maginawindeMac-mini.local>
2015-01-30 17:16:29 +0800

    从第三个直接跳转到首个.

---
+++ CustomSegues/FourthViewController.h
@@ -0,0 +1,13 @@
+//
+//  FourthViewController.h
+//  CustomSegues
+//
+//  Created by maginawin on 15-1-30.
+//  Copyright (c) 2015年 mycj.wwd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FourthViewController : UIViewController
+
+@end
---
+++ CustomSegues/FourthViewController.m
@@ -0,0 +1,23 @@
+//
+//  FourthViewController.m
+//  CustomSegues
+//
+//  Created by maginawin on 15-1-30.
+//  Copyright (c) 2015年 mycj.wwd. All rights reserved.
+//
+
+#import "FourthViewController.h"
+
+@interface FourthViewController ()
+
+@end
+
+@implementation FourthViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
